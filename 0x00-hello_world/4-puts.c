d21c79d91439b46e101dd1bc2f67edaf20b3ac58
Yann Adjumany <yvay03@gmail.com>
2023-06-15 14:57:28 -0700

    Updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 
 int main(void)
 {
-	char strng[30];
-	strcpy(strng, "Programming is like building a multilingual puzzle\n");
-	puts(strng);
+	puts("\"Programming is like building a multilingual puzzle");
+	
 	return (0);
 }
