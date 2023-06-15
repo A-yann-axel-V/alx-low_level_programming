4b348b22737cf1f2a45777a0ed804169080e509b
Yann Adjumany <yvay03@gmail.com>
2023-06-15 14:53:43 -0700

    First .c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char strng[30];
+	strcpy(strng, "Programming is like building a multilingual puzzle");
+	puts(strng);
+	return (0);
+}
