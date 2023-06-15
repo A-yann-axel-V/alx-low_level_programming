3819630057bb767c07d0a39e4ec0f6c268b7a067
Yann Adjumany <yvay03@gmail.com>
2023-06-15 16:24:41 -0700

    being a genius - understanding UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char msg[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char msg[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	size_t n = strlen(msg);
 
 	write(STDOUT_FILENO, msg, n);
