fa4cf5cf7564d98c4fb81d32caf342b76738012a
Yann Adjumany <yvay03@gmail.com>
2023-06-16 18:36:49 -0700

    Alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[27] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (i < 27)
+	{
+		if (alphabet[i] != 'q' && alphabet[i] != 'e')
+			putchar(alphabet[i]);
+	}
+
+	return (0);
+}
