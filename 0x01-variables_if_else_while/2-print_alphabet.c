aed3816387005a3621939bdd39f0f6a79996d643
Yann Adjumany <yvay03@gmail.com>
2023-06-16 18:06:24 -0700

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	char alphabet[26];
+
+	strcpy(alphabet, "abcdefghijklmnopqrstuvwxyz");
+	while (i < 26)
+	{
+		putchar(alphabet[i]);
+		i += 1;
+	}
+
+	return (0);
+}
