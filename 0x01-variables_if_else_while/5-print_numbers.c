0862c6a84af42bfcf82ce7da09b26122f52b6be0
Yann Adjumany <yvay03@gmail.com>
2023-06-16 18:43:25 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <sdtio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	while (i < 10)
+	{
+		printf("%d", i + 1);
+		i += 1;
+	}
+}
