bfa8e084bd8d2d6feed56242a658672fb80c003f
Yann Adjumany <yvay03@gmail.com>
2023-06-20 19:01:41 -0700

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,16 +13,24 @@ void print_to_98(int n)
 
 	if (n < 98)
 	{
-		for (i = n; i < 98; i++)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d,", i);
+			if (i != 98)
+			{
+				_putchar(' ');
+			}
 		}
 	}
 	else if (n > 98)
 	{
-		for (i = n; i > 98; i--)
+		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			printf("%d,", i);
+			if (i != 98)
+			{
+				_putchar(' ');
+			}
 		}
 	}
 	if (n == 98)
