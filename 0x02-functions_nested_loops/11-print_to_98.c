24f7bd8840eeb2c375d50cdefd3f848e3e997cc8
Yann Adjumany <yvay03@gmail.com>
2023-06-20 19:03:41 -0700

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,10 +15,10 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d,", i);
+			printf("%d", i);
 			if (i != 98)
 			{
-				_putchar(' ');
+				printf(", ");
 			}
 		}
 	}
@@ -26,10 +26,10 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d,", i);
+			printf("%d", i);
 			if (i != 98)
 			{
-				_putchar(' ');
+				printf(", ");
 			}
 		}
 	}
