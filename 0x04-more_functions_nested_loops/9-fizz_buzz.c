17b319a58cb8160c11329af26c9811546185fd35
Yann Adjumany <yvay03@gmail.com>
2023-06-23 15:09:33 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 100, j;
+	char strng[] = "FizzBuzz";
+
+	while (n > 0)
+	{
+		if (n % 3 == 0)
+		{
+			for (j = 0; j < 4; j++)
+			{
+				_putchar(strng[j]);
+			}
+		}
+		else if (n % 5 == 0)
+		{
+			for (j = 4; j < 8; j++)
+			{	
+				_putchar(strng[j]);
+			}
+		}
+		else if (n % 3 == 0 && n % 5 == 0)
+		{
+			for (j = 0; j < 8; j++)
+			{
+				_putchar(strng[j]);
+			}
+		}
+		else
+			_putchar(n + '0');
+		i--;
+	}
+
+	return (0);
+}
