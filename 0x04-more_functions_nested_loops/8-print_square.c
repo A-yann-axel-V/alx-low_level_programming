7d07d6f877db949072cf713abfa19a6225ab72c7
Yann Adjumany <yvay03@gmail.com>
2023-06-22 19:23:02 -0700

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,13 +8,16 @@
 void print_square(int size)
 {
 	int k = 0, l;
-
-	for (; k < n; k++)
-	{
-		for (l = 0; l < n; l++)
-		{
-			_putchar(35);
-		}
+	
+	if (size <= 0)
 		_putchar('\n');
-	}
+	else
+		for (; k < n; k++)
+		{
+			for (l = 0; l < n; l++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
 }
