09deedc97a9b312a45d78191802048089bc640c2
Yann Adjumany <yvay03@gmail.com>
2023-06-23 15:25:08 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -8,36 +8,47 @@
 
 int main(void)
 {
-	int n = 100, j;
+	int n = 1, j;
 	char strng[] = "FizzBuzz";
 
-	while (n > 0)
+	while (n < 101)
 	{
-		if (n % 3 == 0)
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			for (j = 0; j < 8; j++)
+			{
+				putchar(strng[j]);
+			}
+		}
+		else if (n % 3 == 0)
 		{
 			for (j = 0; j < 4; j++)
 			{
-				_putchar(strng[j]);
+				putchar(strng[j]);
 			}
 		}
 		else if (n % 5 == 0)
 		{
 			for (j = 4; j < 8; j++)
 			{
-				_putchar(strng[j]);
-			}
-		}
-		else if (n % 3 == 0 && n % 5 == 0)
-		{
-			for (j = 0; j < 8; j++)
-			{
-				_putchar(strng[j]);
+				putchar(strng[j]);
 			}
 		}
 		else
-			_putchar(n + '0');
-		i--;
+		{
+			if (n < 10)
+				putchar(n + '0');
+			else
+			{
+				putchar((n / 10) + '0');
+				putchar((n % 10) + '0');
+			}
+		}
+		if (n != 100)
+			putchar(' ');
+		n++;
 	}
+	putchar('\n');
 
 	return (0);
 }
