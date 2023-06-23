b0d5c6baa57815c19f00ddde0b8d760ac5d42844
Yann Adjumany <yvay03@gmail.com>
2023-06-22 18:02:22 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,16 @@
 void more_numbers(void)
 {
 	int i, j;
-
-	for (i = 0; i < 10; i++)
+	
+	i = 0;
+	while (i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
 			if (j > 9)
+			{
 				_putchar((j / 10) + '0');
+			}
 			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
