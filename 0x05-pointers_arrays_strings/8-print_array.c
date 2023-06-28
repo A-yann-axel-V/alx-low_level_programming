ffc1f158ef76390e273dfdb570c1c5a0e319b00c
Yann Adjumany <yvay03@gmail.com>
2023-06-27 17:23:55 -0700

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,6 +12,13 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-		_putchar('0' + a[i]);
+	{
+		_putchar(a[i] + '0');
+		if (i != n - 1)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
 	_putchar('\n');
 }
