1b1541ece01d39880e00af7fc8a887b8b671d206
Yann Adjumany <yvay03@gmail.com>
2023-06-29 19:10:49 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: Destination
+ * @src: Source
+ * @n: n as Integer
+ * Return: Pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count = 0;
+
+	while (*src != '\0' && count < n)
+	{
+		*dest = *src;
+		*dest++;
+		*src++;
+		count++;
+	}
+	*dest = '\0';
+
+	return (dest);
+}
