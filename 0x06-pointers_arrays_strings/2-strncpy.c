d0538f447d27694eeecbafbb2b7d79d71e7cd9c5
Yann Adjumany <yvay03@gmail.com>
2023-06-29 19:35:37 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,21 +10,16 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int count = 0;
+	int count;
 
-	while (src[count] != '\0' && n > 0)
+	for (count = 0;count < n && src[count] != '\0'; count++)
 	{
 		dest[count] = src[count];
-		count++;
-		n--;
 	}
-	
-	count = 0;
-	while (n > 0)
+
+	for (; count < n; count++)
 	{
 		dest[count] = '\0';
-		count++;
-		n--;
 	}
 
 	return (dest);
