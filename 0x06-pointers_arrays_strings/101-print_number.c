8bc1f666600472dd94392aec5f17346192042d70
Yann Adjumany <yvay03@gmail.com>
2023-06-30 17:51:18 -0700

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: int
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	int k = 1, x, value;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	x = n;
+
+	while (x / 10 != 0)
+	{
+		k *= 10;
+		x = x / 10;
+	}
+
+	while (k / 10 != 0)
+	{
+		value = n / k;
+		_putchar(value + '0');
+		n %= k;
+	}
+}
