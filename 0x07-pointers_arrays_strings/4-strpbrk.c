dd365678a9aff0446c7a55f43315e16149699f0e
Yann Adjumany <yvay03@gmail.com>
2023-07-04 15:15:09 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,15 +13,18 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	while (*s)
+	int i = 0, j;
+
+	while (s[i] != '\0')
 	{
-		while (*accept)
+		j = 0;
+		while (accept[j] != '\0')
 		{
-			if (*s == *accept)
-				return (s);
-			accept++;
+			if (s[i] == accept[j])
+				return (s[i]);
+			j++;
 		}
-		s++;
+		i++;
 	}
 
 	return (NULL);
