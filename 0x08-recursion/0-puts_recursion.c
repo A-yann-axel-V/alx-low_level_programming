2e99293af0945d9aa6dd30090cfea063d7c641d5
Yann Adjumany <yvay03@gmail.com>
2023-07-05 17:15:26 -0700

    new project

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: String to be printed
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	int k = 0;
+
+	if (s[k] != '\0')
+	{
+		_putchar(s[k]);
+		_puts_recursion(s + 1);
+	}
+}
