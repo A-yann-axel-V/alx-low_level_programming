ad917be0921e428a86dd5b58046fd9a4c7950e49
Yann Adjumany <yvay03@gmail.com>
2023-07-13 12:54:30 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,10 +33,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i]; i++)
+	for (i = 0; i < len1; i++)
 		ptr[i] = s1[i];
 
-	for (i = 0; s2[i] && i < n; i++)
+	for (i = 0; i < n; i++)
 		ptr[len1 + i] = s2[i];
 
 	ptr[len1 + len2] = '\0';
