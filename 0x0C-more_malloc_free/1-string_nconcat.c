a03542887b9d16a74983aaa33f08c546b47ec0d6
Yann Adjumany <yvay03@gmail.com>
2023-07-13 12:27:13 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,8 +14,8 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	int i = 0, j, len, len1 = 0, len2 = 0;
-	
+	unsigned int i = 0, j, len, len1 = 0, len2 = 0;
+
 	if (s1 != NULL)
 	{
 		while (s1[len1] != '\0')
