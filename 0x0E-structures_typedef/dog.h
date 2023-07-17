6b60eddf0255fb5624d4f61f89aba03d638161e8
Yann Adjumany <yvay03@gmail.com>
2023-07-17 05:51:00 -0700

    doggy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure or class named dog
+ *
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: The owner of the dog
+ */
+
 struct dog
 {
 	char *name;
