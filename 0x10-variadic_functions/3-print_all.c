7e25d039523d44479ff63fcf0337d58e4296800a
Yann Adjumany <yvay03@gmail.com>
2023-07-20 19:37:48 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,55 +1,48 @@
 #include "variadic_functions.h"
-#include <stddef.h>
-#include <stdio.h>
 
 /**
  * print_all - prints anything
  * @format: list of types of arguments passed to the function
- *
  * Return: nothing
  */
+
 void print_all(const char * const format, ...)
 {
-	int count = 0, i = 0, put_sep;
+	int i = 0, put_sep;
 	va_list args;
 	char *value;
 
 	va_start(args, format);
-	if (format != NULL)
+	while (format != NULL && format[i] != '\0')
 	{
-		while (format[count])
-			count++;
-		while (i < count)
+		switch (format[i])
 		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%c", va_arg(args, int));
-					put_sep = 0;
-					break;
-				case 'i':
-					printf("%i", va_arg(args, int));
-					put_sep = 0;
-					break;
-				case 'f':
-					printf("%f", va_arg(args, double));
-					put_sep = 0;
-					break;
-				case 's':
-					value = va_arg(args, char*);
-					if (value == NULL)
-						value = "(nil)";
-					printf("%s", value);
-					put_sep = 0;
-					break;
-				default:
-					put_sep = 1;
-					break;
-			}
-			if (i != count - 1 && put_sep == 0)
-				printf(", ");
-			i++;
+			case 'c':
+				printf("%c", va_arg(args, int));
+				put_sep = 0;
+				break;
+			case 'i':
+				printf("%i", va_arg(args, int));
+				put_sep = 0;
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				put_sep = 0;
+				break;
+			case 's':
+				value = va_arg(args, char*);
+				if (value == NULL)
+					value = "(nil)";
+				printf("%s", value);
+				put_sep = 0;
+				break;
+			default:
+				put_sep = 1;
+				break;
 		}
+		if (i != count - 1 && put_sep == 0)
+			printf(", ");
+		i++;
 	}
 	printf("\n");
 	va_end(args);
