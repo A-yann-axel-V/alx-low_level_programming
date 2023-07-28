f94034ea5a81c04b41827a5ff7febf48b20580ce
Yann Adjumany <yvay03@gmail.com>
2023-07-27 19:56:54 -0700

    beofre main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,13 +3,13 @@
 /**
  * msg_before_main - Function executed before main()
  */
-void msg_before_main() __attribute__((constructor));
+void msg_before_main(void) __attribute__((constructor));
 
 /**
  * msg_before_main - prints a message
  */
-void msg_before_main()
+void msg_before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n\
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
