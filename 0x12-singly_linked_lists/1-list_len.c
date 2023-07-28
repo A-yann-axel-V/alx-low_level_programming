48b4b30cb91491da435411fda064c7817032f5a8
Yann Adjumany <yvay03@gmail.com>
2023-07-27 18:23:24 -0700

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * list_len - count the number of elements
+ * in a linked list_t list
+ * @h: a linked list
+ *
+ * Return: the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
