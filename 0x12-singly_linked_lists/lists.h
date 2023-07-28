7d35864ac5c20d3e1574b75150dc98fb6da2297e
Yann Adjumany <yvay03@gmail.com>
2023-07-27 18:18:27 -0700

    print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,13 +4,12 @@
 #include <stddef.h>
 
 /**
- * struct list - Singly linked list
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: singly linked list 
- * node structure
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
