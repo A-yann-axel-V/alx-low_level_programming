41dbaead4309291645f653512e194803bbc656c6
Yann Adjumany <yvay03@gmail.com>
2023-07-31 09:52:10 -0700

    more linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: linked list
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h->next)
+	{
+		printf("%d", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
