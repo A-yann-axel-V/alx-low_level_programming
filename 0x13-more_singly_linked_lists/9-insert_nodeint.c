07be11acbc7f6b6149d65cdf522e820fe6cf00fd
Yann Adjumany <yvay03@gmail.com>
2023-08-01 10:10:49 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx,
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	if (n == 0)
+	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
