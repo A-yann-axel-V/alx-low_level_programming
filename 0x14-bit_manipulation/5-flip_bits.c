1ec34b7805a668c2853f82d8316760ec09e20057
Yann Adjumany <yvay03@gmail.com>
2023-08-03 17:51:33 -0700

    flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -20,7 +20,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	for (i = 0; i < MAX; i++)
 	{
-		if (num == (sub & result))
+		if (num == (sub & num))
 			count++;
 		num <<= 1;
 	}
