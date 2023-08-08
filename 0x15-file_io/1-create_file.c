ba7682ef0ac00702b6139637bb4fe9fc23a55ad7
Yann Adjumany <yvay03@gmail.com>
2023-08-07 17:42:49 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,11 +24,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content[len] != '\0')
+		while (text_content[len])
 			len++;
 	}
 
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, len);
 
 	if (op == -1 || wr == -1)
