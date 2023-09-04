a74713c5dca919edb2d96162c0c8f7fabf41cfab
Yann Adjumany <yvay03@gmail.com>
2023-09-04 15:25:23 -0700

    files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+void check(ssize_t res, char *buf, int f_from, int f_to, char *msg,
+		int value, int n);
 
 #endif /* MAIN_H */
