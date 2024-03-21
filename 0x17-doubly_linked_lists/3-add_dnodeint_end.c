b465b84f038fb016a42833487237160d7c0080ca
Yann Adjumany <yvay03@gmail.com>
2024-03-21 16:56:37 -0700

    append d node

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -5,7 +5,7 @@
  * @head: head of the d linked list
  * @n: value of the new node
  *
- * Return:  address of the new element, or NULL if it failed.
+ * Return: address of the new element, or NULL if it failed.
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
-        new_node->next = NULL;
+	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
