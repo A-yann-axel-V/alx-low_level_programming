8be882cd261209eb099b410241ff6e993c6fd7c7
Yann Adjumany <yvay03@gmail.com>
2024-03-22 19:03:50 -0700

    delete node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,12 +15,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node = NULL, *current = *h;
 	size_t len = 0;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-
 	if (h == NULL || idx == 0)
 		return (add_dnodeint(h, n));
 
@@ -31,17 +25,20 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	if (len < idx)
-	{
-		free(new_node);
 		return (NULL);
-	}
 	else if (idx == (len - 1))
 		return (add_dnodeint_end(h, n));
 
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+
 	current = current->prev;
 	new_node->next = current->next;
+	if (current->next != NULL)
+		current->next->prev = new_node;
 	new_node->prev = current;
-	current->next->prev = new_node;
 	current->next = new_node;
 
 	return (new_node);
