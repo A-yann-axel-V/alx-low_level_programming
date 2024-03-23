48ce2abbd47e6457b44c3407a767e84b34518028
Yann Adjumany <yvay03@gmail.com>
2024-03-22 19:07:13 -0700

    check

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -33,7 +33,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		i++;
 	}
 
-	if (temp == NULL)
+	if (temp == NULL || i < index)
 		return (-1);
 
 	temp->prev->next = temp->next;
