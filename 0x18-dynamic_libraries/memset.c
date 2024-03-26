9a162bb76e86e203b4aa4ecd4ff5d0418f26b824
Yann Adjumany <yvay03@gmail.com>
2024-03-26 03:07:04 +0000

    lib

---
+++ 0x18-dynamic_libraries/memset.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of the memory area
+ * pointed to by s with the constant byte b
+ *
+ * @s: Parameter
+ * @b: Parameter
+ * @n: Parameter as int
+ * Return: pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int k = 0;
+
+	while (k < n)
+	{
+		s[k] = b;
+		k++;
+	}
+
+	return (s);
+}
---
+++ 0x18-dynamic_libraries/strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strpbrk - locates the first occurrence in the string @s
+ * of any of the bytes in the string @accept
+ *
+ * @s: String to be scanned;
+ * @accept: String containing the characters to match
+ *
+ * Return: Pointer
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int j;
+
+	while (*s)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+				return (s);
+		}
+		s++;
+	}
+
+	return (NULL);
+}
