14b8cca2252b8539124e2fc7ad867ca0c1fa10ba
Yann Adjumany <yvay03@gmail.com>
2024-03-27 04:21:00 -0700

    operations lib

---
+++ 0x18-dynamic_libraries/operators.c
@@ -0,0 +1,61 @@
+#include "op.h"
+
+/**
+ * add - Addition
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: a plus b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Substraction
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: a minus b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplication
+ * @a: an integer
+ * @b: an integer
+ *
+ * Return: a times b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Division
+ * @a: An integer
+ * @b: An integer
+ *
+ * Return: a divided by b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - Modulo
+ * @a: An integer
+ * @b: An integer
+ *
+ * Return: a modulo b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
